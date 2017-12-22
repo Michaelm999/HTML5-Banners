b83d7e7b9b7cec1f52a68c304470f6cf9a2ab899
Michael Mazzarella <michael_mazzarella@hotmail.com>
2017-12-22 11:40:36 -0800

    redesigned banner

--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,14 @@
-$(document).ready(function() {
-  setInterval(rotateImages, 4000);
-});
+window.onload = rotate;
 
-function rotateImages(){
-  var curPhoto = $("photoShow div.currentImage");
-  var nxtPhoto = curPhoto.next();
-  
-  if(nxtPhoto.length == 0){
-    nxtPhoto = $("photoShow div:first"); 
-  }
-  
-  curPhoto.removeClass("current").addClass("previous");
-  nxtPhoto.css({opacity: 0.0}).addClass("current").animate({opacity: 1.0}, 1000,
-                                                           
-  function(){
-    curPhoto.removeClass("previous");
-  });
+var adImages = new Array("https://dribbble.s3.amazonaws.com/users/4467/screenshots/257352/abraham-lewis.jpg","https://dribbble.s3.amazonaws.com/users/4467/screenshots/245651/robot.jpg","https://dribbble.s3.amazonaws.com/users/4467/screenshots/212240/brainoid.jpg");
+var thisAd = 0;
+
+function rotate() {
+	thisAd++;
+	if (thisAd == adImages.length) {
+		thisAd = 0;
+	}
+	document.getElementById("adBanner").src = adImages[thisAd];
+
+	setTimeout("rotate()", 3 * 500);
 }
